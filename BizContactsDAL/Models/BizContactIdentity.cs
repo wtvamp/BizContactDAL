55da4bbac69bb4b5fe99dcd71b6a0667e211bfdb
Warren J Thompson <warrenjfthompson@outlook.com>
2018-11-20 11:53:41 -0800

    Adding idenity model for user

---
+++ BizContactsDAL/Models/BizContactIdentity.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BizContacts.DAL
+{
+    class BizContactIdentityContext: DbContext
+    {
+        public BizContactIdentityContext(DbContextOptions<BizContactContext> options)
+            : base(options)
+        { }
+
+        public DbSet<BizContactIdentity> BizContactIdentities { get; set; }
+    }
+
+    public class BizContactIdentity: IdentityUser
+    {
+        // Extended Properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
